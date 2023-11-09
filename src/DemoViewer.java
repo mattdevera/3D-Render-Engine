f72a3b6f24ea022c327361aa984bfd00616904dc
Matthew De Vera <deveramatthew1@gmail.com>
2023-11-09 12:07:14 -0800

    Implemented GUI wrapper

--- src/DemoViewer.java
+++ src/DemoViewer.java
@@ -3,4 +3,33 @@ import java.awt.*;
 
 public class DemoViewer {
     
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        Container pane = frame.getContentPane();
+        pane.setLayout(new BorderLayout());
+
+        //slider for horizontal rotation
+        JSlider headingSlider = new JSlider(1, 360, 180);
+        pane.add(headingSlider, BorderLayout.SOUTH);
+
+        //slider for vertical rotation
+        JSlider pitchSlider = new JSlider(SwingConstants.VERTICAL, -90, 90, 0);
+        pane.add(pitchSlider, BorderLayout.EAST);
+
+        //panel to display render results
+        JPanel renderPanel = new JPanel() {
+            public void paintComponent(Graphics g) {
+                Graphics2D g2 = (Graphics2D) g;
+                g2.setColor(Color.BLACK);
+                g2.fillRect(0, 0, getWidth(), getHeight());
+                 
+                //render implementation goes here
+
+            }
+        };
+        pane.add(renderPanel, BorderLayout.CENTER);
+
+        frame.setSize(400, 400);
+        frame.setVisible(true);
+    }
 }
