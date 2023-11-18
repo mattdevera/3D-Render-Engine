7214b14b3aff40c3d25020b0b02868f08b5a2c50
Matthew De Vera <deveramatthew1@gmail.com>
2023-11-18 11:28:58 -0800

    Implented Vertex and Triangle class

--- src/DemoViewer.java
+++ src/DemoViewer.java
@@ -33,3 +33,31 @@ public class DemoViewer {
         frame.setVisible(true);
     }
 }
+
+class Vertex {
+    
+    double x;
+    double y;
+    double z;
+    
+    Vertex(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
+
+class Triangle {
+
+    Vertex v1;
+    Vertex v2;
+    Vertex v3;
+    Color color;
+
+    Triangle(Vertex v1, Vertex v2, Vertex v3, Color color) {
+        this.v1 = v1;
+        this.v2 = v2;
+        this.v3 = v3;
+        this.color = color;
+    }
+}
