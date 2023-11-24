cb96c89126f643cf0c4653ed5faca71ba5624954
Matthew De Vera <deveramatthew1@gmail.com>
2023-11-24 13:01:23 -0800

    Implemented render for tetrahedron template

--- src/DemoViewer.java
+++ src/DemoViewer.java
@@ -1,5 +1,8 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.Path2D;
+import java.util.List;
+import java.util.ArrayList;
 
 public class DemoViewer {
     
@@ -22,9 +25,24 @@ public class DemoViewer {
                 Graphics2D g2 = (Graphics2D) g;
                 g2.setColor(Color.BLACK);
                 g2.fillRect(0, 0, getWidth(), getHeight());
-                 
+                
                 //render implementation goes here
+                List<Triangle> tris = new ArrayList<>();
+                tris.add(new Triangle(new Vertex(100, 100, 100), new Vertex(-100, -100, 100), new Vertex(-100, 100, -100), Color.WHITE));
+                tris.add(new Triangle(new Vertex(100, 100, 100),  new Vertex(-100, -100, 100), new Vertex(100, -100, -100), Color.RED));
+                tris.add(new Triangle(new Vertex(-100, 100, -100),  new Vertex(100, -100, -100), new Vertex(100, 100, 100), Color.GREEN));
+                tris.add(new Triangle(new Vertex(-100, 100, -100),  new Vertex(100, -100, -100), new Vertex(-100, -100, 100), Color.BLUE));
 
+                g2.translate(getWidth() / 2, getHeight() / 2);
+                g2.setColor(Color.WHITE);
+                for (Triangle t : tris) {
+                    Path2D path = new Path2D.Double();
+                    path.moveTo(t.v1.x, t.v1.y);
+                    path.lineTo(t.v2.x, t.v3.y);
+                    path.lineTo(t.v3.x, t.v3.y);
+                    path.closePath();
+                    g2.draw(path);
+                }
             }
         };
         pane.add(renderPanel, BorderLayout.CENTER);
