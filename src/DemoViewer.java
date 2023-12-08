6e590dceb106ec3e468ceac33e89b56a786dd83c
Matthew De Vera <deveramatthew1@gmail.com>
2023-12-08 11:13:03 -0800

    Implemented depth to the render.

--- src/DemoViewer.java
+++ src/DemoViewer.java
@@ -91,6 +91,11 @@ public class DemoViewer {
                 //rasterizing implementation to fill in the 3D object
                 BufferedImage img = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_ARGB);
 
+                //introducing depth to the 3D object via z-buffer
+                double[] zBuffer = new double[img.getWidth() * img.getHeight()];
+                for(int i = 0; i < zBuffer.length; i++)
+                    zBuffer[i] = Double.NEGATIVE_INFINITY;
+
                 for(Triangle t : tris) {
                     Vertex v1 = transform.transform(t.v1);
                     Vertex v2 = transform.transform(t.v2);
@@ -118,8 +123,16 @@ public class DemoViewer {
                             double b2 = ((y - v1.y) * (v3.x - v1.x) + (v3.y - v1.y) * (v1.x - x)) / triangleArea;
                             double b3 = ((y - v2.y) * (v1.x - v2.x) + (v1.y - v2.y) * (v2.x - x)) / triangleArea;
 
-                            if(b1 >= 0 && b1 <= 1 && b2 >= 0 && b2 <= 1 && b3 >= 0 && b3 <= 1)
-                                img.setRGB(x, y, t.color.getRGB());
+                            if(b1 >= 0 && b1 <= 1 && b2 >= 0 && b2 <= 1 && b3 >= 0 && b3 <= 1) {
+                                //depth rendering
+                                double depth = b1 * v1.z + b2 * v2.z + b3 * v3.z;
+                                int zIndex = y * img.getWidth() + x;
+                                if(zBuffer[zIndex] < depth) {
+                                    img.setRGB(x, y, t.color.getRGB());
+                                    zBuffer[zIndex] = depth;
+                                }
+                            }
+
                         }
                     }
                 }
