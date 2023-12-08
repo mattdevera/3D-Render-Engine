9962fb1e4d81d6304c9697b2eff1b83dc4b8ed57
Matthew De Vera <deveramatthew1@gmail.com>
2023-12-08 11:04:56 -0800

    Implemented rasterization of the object.

--- src/DemoViewer.java
+++ src/DemoViewer.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.geom.Path2D;
+import java.awt.image.BufferedImage;
 import java.util.List;
 import java.util.ArrayList;
 
@@ -22,19 +23,19 @@ public class DemoViewer {
         //panel to display render results
         JPanel renderPanel = new JPanel() {
             public void paintComponent(Graphics g) {
+                //set up for panel rendering
                 Graphics2D g2 = (Graphics2D) g;
                 g2.setColor(Color.BLACK);
                 g2.fillRect(0, 0, getWidth(), getHeight());
-                
-                //render implementation goes here
-                
+
+                //creation of the 3D object instance
                 List<Triangle> tris = new ArrayList<>();
                 tris.add(new Triangle(new Vertex(100, 100, 100), new Vertex(-100, -100, 100), new Vertex(-100, 100, -100), Color.WHITE));
                 tris.add(new Triangle(new Vertex(100, 100, 100),  new Vertex(-100, -100, 100), new Vertex(100, -100, -100), Color.RED));
                 tris.add(new Triangle(new Vertex(-100, 100, -100),  new Vertex(100, -100, -100), new Vertex(100, 100, 100), Color.GREEN));
                 tris.add(new Triangle(new Vertex(-100, 100, -100),  new Vertex(100, -100, -100), new Vertex(-100, -100, 100), Color.BLUE));
                 
-                /*
+                /* former render implementation for the engine
                 g2.translate(getWidth() / 2, getHeight() / 2);
                 g2.setColor(Color.WHITE);
                 for (Triangle t : tris) {
@@ -45,12 +46,7 @@ public class DemoViewer {
                     path.closePath();
                     g2.draw(path);
                 }
-                */
 
-                //transformation implementation
-                double heading = Math.toRadians(headingSlider.getValue());
-
-                /*
                 Matrix3 transform = new Matrix3(new double[] {
                     Math.cos(heading), 0, -Math.sin(heading),
                     0, 1, 0,
@@ -58,8 +54,8 @@ public class DemoViewer {
                 });
                 */
 
-                
-
+                //x and y coordinate transformation implementation
+                double heading = Math.toRadians(headingSlider.getValue());
                 double pitch = Math.toRadians(pitchSlider.getValue());
 
                 Matrix3 headingTransform = new Matrix3(new double[] {
@@ -74,7 +70,9 @@ public class DemoViewer {
                 });
 
                 Matrix3 transform = headingTransform.multiply(pitchTransform);
-                
+
+                //implementation for object visualization via wireframing
+                /*
                 g2.translate(getWidth() / 2, getHeight() / 2);
                 g2.setColor(Color.WHITE);
                 for(Triangle t : tris) {
@@ -88,8 +86,48 @@ public class DemoViewer {
                     path.closePath();
                     g2.draw(path);
                 }
+                */
+
+                //rasterizing implementation to fill in the 3D object
+                BufferedImage img = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_ARGB);
+
+                for(Triangle t : tris) {
+                    Vertex v1 = transform.transform(t.v1);
+                    Vertex v2 = transform.transform(t.v2);
+                    Vertex v3 = transform.transform(t.v3);
+
+                    //manual translation since we are no longer using the Graphics2D class
+                    v1.x += getWidth() / 2;
+                    v1.y += getHeight() / 2;
+                    v2.x += getWidth() / 2;
+                    v2.y += getHeight() / 2;
+                    v3.x += getWidth() / 2;
+                    v3.y += getHeight() / 2;
+
+                    //computation for rectangular bounds of the 3D object
+                    int minX = (int)Math.max(0, Math.ceil(Math.min(v1.x, Math.min(v2.x, v3.x))));
+                    int maxX = (int)Math.min(img.getWidth() - 1, Math.floor(Math.max(v1.x, Math.max(v2.x, v3.x))));
+                    int minY = (int)Math.max(0, Math.ceil(Math.min(v1.y, Math.min(v2.y, v3.y))));
+                    int maxY = (int)Math.min(img.getHeight() - 1, Math.floor(Math.max(v1.y, Math.max(v2.y, v3.y))));
+
+                    double triangleArea = (v1.y - v3.y) * (v2.x - v3.x) + (v2.y - v3.y) * (v3.x - v1.x);
+
+                    for(int y = minY; y <= maxY; y++) {
+                        for(int x = minX; x <= maxX; x++) {
+                            double b1 = ((y - v3.y) * (v2.x - v3.x) + (v2.y - v3.y) * (v3.x - x)) / triangleArea;
+                            double b2 = ((y - v1.y) * (v3.x - v1.x) + (v3.y - v1.y) * (v1.x - x)) / triangleArea;
+                            double b3 = ((y - v2.y) * (v1.x - v2.x) + (v1.y - v2.y) * (v2.x - x)) / triangleArea;
+
+                            if(b1 >= 0 && b1 <= 1 && b2 >= 0 && b2 <= 1 && b3 >= 0 && b3 <= 1)
+                                img.setRGB(x, y, t.color.getRGB());
+                        }
+                    }
+                }
+                g2.drawImage(img, 0, 0, null);
             }
         };
+
+        //implementation for user interaction and rendering computation listeners
         pane.add(renderPanel, BorderLayout.CENTER);
         headingSlider.addChangeListener(e -> renderPanel.repaint());
         pitchSlider.addChangeListener(e -> renderPanel.repaint());
